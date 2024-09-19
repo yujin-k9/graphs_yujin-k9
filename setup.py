66aa0d47af43bf784099489c59df3275da2981cc
yujin-k9 <ykim28627@gmail.com>
2024-09-19 08:43:37 -0600

    Updated files

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="graphs_yujin_k9",
-    version="0.1",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
+    version="0.0.1",
+    description="A Python package for Dijkstra's algorithm and other graph-related algorithms.",
+    author="Kaylee Kim",
+    author_email="ykim28627@gmail.com",
+    packages=["graphs_yujin_k9"],
+    install_requires=[],
 )
